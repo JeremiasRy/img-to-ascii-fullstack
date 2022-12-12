cacffaee962d126c5fccb9dff3f260571c1fc32f
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-12 11:30:28 +0200

    refactor: img to ascii conversion task

--- backend/ImgToASCIII/ImgToASCIIConverter.cs
+++ backend/ImgToASCIII/ImgToASCIIConverter.cs
@@ -10,29 +10,31 @@ public class ImgToASCIIConverter
     readonly float _width;
     readonly string _grayScale = "@%#*+=-:. ";
     readonly float _grayScaleMultiplier;
+    List<string> _rows = new();
 
+    [SupportedOSPlatform("windows")]
+    void Rows()
+    {
+        List<string> rows = new();
+
+        for (int iY = 0; iY < _height; iY++)
+        {
+            string row = "";
+            for (int iX = 0; iX < _width; iX++)
+            {
+                Color pixel = _image.GetPixel(iX, iY);
+                float pixelGrayScale = (pixel.R + pixel.G + pixel.B) / 3;
+                row += _grayScale.ElementAt((int)Math.Round(_grayScaleMultiplier * pixelGrayScale));
+            }
+            rows.Add(row);
+        }
+        _rows = rows;
+    }
     [SupportedOSPlatform("windows")]
     public async Task<AsciiPicture> GetAsciiPicture()
     {
-        AsciiPicture? asciiPicture = null;
-        await Task.Run(() =>
-        {
-            List<string> rows = new();
-
-            for (int iY = 0; iY < _height; iY++)
-            {
-                string row = "";
-                for (int iX = 0; iX < _width; iX++)
-                {
-                    Color pixel = _image.GetPixel(iX, iY);
-                    float pixelGrayScale = (pixel.R + pixel.G + pixel.B) / 3;
-                    row += _grayScale.ElementAt((int)Math.Round(_grayScaleMultiplier * pixelGrayScale));
-                }
-                rows.Add(row);
-            }
-            asciiPicture = new AsciiPicture(rows.ToArray());
-        });
-        return asciiPicture == null ? new AsciiPicture(Array.Empty<string>()) : asciiPicture;
+        await Task.Run(() => Rows());
+        return new AsciiPicture(_rows.ToArray());
     }
     public ImgToASCIIConverter(Stream image)
     {
