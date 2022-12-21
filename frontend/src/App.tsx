e581d37882f9e4cb45a2ce9624a6b7a699dd32fa
JeremiasRy <ryttari.jeremias@gmail.com>
2022-12-21 09:34:34 +0200

    feat: output size logic calculations

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,36 +1,43 @@
-import { useState, ChangeEvent, useRef, SyntheticEvent } from "react";
+import { useState, useRef, SyntheticEvent, useEffect } from "react";
 import fileService from "./services/fileService";
 import "./App.css";
 import Picture from "./types/Picture";
 
+interface IOutputelement {
+  width:number,
+  height:number
+}
 function App() {
 
   const [ picture, setPicture ] = useState<Picture | null>(null);
   const [ file, setFile ] = useState<Blob | null>(null);
   const [ divClassName, setDivClassName ] = useState<"max-size" | "fit-picture">("max-size")
+  const [ outputElement, setOutputElement ] = useState<IOutputelement>({width: 0, height: 0})
   const element = useRef(null);
 
-  let styles:CSSStyleDeclaration;
-  let expectedWidth = "";
-  let expectedHeight = "";
-
-  if (element.current !== null) {
-    styles = getComputedStyle(element.current);
-    expectedHeight = String(Math.floor(parseInt(styles.height.replace('px', '')) / 10));
-    expectedWidth = String(Math.floor(parseInt(styles.width.replace('px', '')) / parseInt(styles.fontSize.replace('px', '')))); 
-  }
+  
+  useEffect(() => {
+    if (element.current !== null) {
+      let styles:CSSStyleDeclaration;
+      styles = getComputedStyle(element.current);
+      let outputElement:IOutputelement = {
+        width: Math.floor(parseInt(styles.width.replace('px', '')) / parseInt(styles.fontSize.replace('px', ''))),
+        height: Math.floor(parseInt(styles.height.replace('px', '')) / 10)
+      }
+      setOutputElement(outputElement);
+    }
+  }, []) ;
+  
 
   async function handleSubmit(e:SyntheticEvent) {
     e.preventDefault();
 
     if (element.current !== null && file !== null) { 
-
       const imgFormData = new FormData();
 
       imgFormData.append("image", file);
-      imgFormData.append("width", expectedWidth);
-      imgFormData.append("height", expectedHeight)
-      console.log(Math.floor(parseInt(styles.height.replace('px', '')) / 10))
+      imgFormData.append("width", String(outputElement.width));
+      imgFormData.append("height", String(outputElement.height))
       let imageData = await fileService.postImg(imgFormData);
       setPicture(new Picture(imageData.height, imageData.width, imageData.rows));
       setFile(null);
